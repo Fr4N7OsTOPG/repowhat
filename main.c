18690a7c604d06a2e999dab420c35f0c830243bd
Fr4N7OsTOPG <114771993+Fr4N7OsTOPG@users.noreply.github.com>
2022-09-30 22:55:42 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+char message[20] = "";
+char symbol;
+int main() {
+    printf("Please, enter your line\n");
+    scanf("%s", &message);
+    printf("There is your line: %s\n", message);
+    printf("Now enter your symbol\n");
+    scanf("%s", &symbol);
+    printf("Here is your symbol: %c", symbol);
+}
