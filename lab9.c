04b3ed6f6973471d627c8f884da20eeba415440d
Damian <114771993+Fr4N7OsTOPG@users.noreply.github.com>
2022-12-29 17:44:30 +0300

    Update lab9.c

--- lab9.c
+++ lab9.c
@@ -20,33 +20,21 @@ int main()
     }else{
         printf("You have spoken %.1f minutes over your limit \nYou have to overpay %.1f rubs. Overpaying rate for 1 minute: %.1f", minutes_spoken - freem, (minutes_spoken - freem)*one_notfree_min, one_notfree_min);
     }
-    int n;
+        int i, N, t, k;
+   
     scanf("%d", &n);
-    int prime[n];
-    int i;
-    for (i = 0; i <= n; i++) 
-    {
-        prime[i] = 1;
-    }
-    int p = 2;
-    while (pow(p, 2) <= n)
-    {
-        if (prime[p] == 1)
-        {
-            for (i = pow(p, 2); p != n + 1; i+=p)
-            {
-                prime[i] = 0;
+    for (k=2; k<=n; k++){
+        i = 2;
+        t = 0;
+        while(i < k){
+            if (k%i==0){
+                t++;
             }
+            i++;
         }
-        p++;
-    }
-    for (p = 2; p != n+1; p++)
-    {
-        if (prime[p] == 1)
-        {
-            printf("%d", p);
+        if(t==0){
+            printf("%d\n", k);
         }
     }
-    
     return 0;
 }
